fe2f04a7351f89535d8b6a27e9a0427b4797a972
Jeremy Kahn <jeremy@jellyvision.com>
2015-12-08 15:33:41 -0600

    Implement pre- and post-processing

--- Gemfile
+++ Gemfile
@@ -4,3 +4,4 @@ source 'https://rubygems.org'
 gemspec
 
 gem 'premailer', '1.8.6'
+gem 'pry', '0.10.3 ruby java'
--- lib/getinline.rb
+++ lib/getinline.rb
@@ -1,16 +1,37 @@
 require "getinline/version"
 require "premailer"
 
+TOKEN = "@TOKEN"
+TOKENIZED_ERB_FILE_NAME = "/tmp/tokenized.erb"
+
 module Getinline
   class Transformer
     def initialize (file_name)
       @file_name = file_name
-      @premailer = Premailer.new(@file_name, :warn_level => Premailer::Warnings::SAFE)
     end
 
-    # FIXME: Implement this method
     def transform
-      @premailer.to_inline_css
+      file = File.open(@file_name, "r")
+      raw_text = file.read
+      file.close
+
+      matches = raw_text.scan(/<%.+?%>/)
+      tokenized_text = raw_text.dup
+      matches.each do |match|
+        tokenized_text.sub!(match, TOKEN)
+      end
+
+      File.write(TOKENIZED_ERB_FILE_NAME, tokenized_text)
+
+      @premailer = Premailer.new(TOKENIZED_ERB_FILE_NAME, :warn_level => Premailer::Warnings::SAFE)
+      premailed_tokenized_text = @premailer.to_inline_css
+      premailed_text = premailed_tokenized_text.dup
+
+      matches.each do |match|
+        premailed_text.sub!(TOKEN, match)
+      end
+
+      premailed_text
     end
   end
 end
