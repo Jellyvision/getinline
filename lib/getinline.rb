0cbcb731083110ae2144c4b7554e2e451cc6c48b
Jeremy Kahn <jeremy@jellyvision.com>
2015-12-08 10:09:08 -0600

    Add stub methods

--- lib/getinline.rb
+++ lib/getinline.rb
@@ -2,5 +2,13 @@ require "getinline/version"
 
 module Getinline
   class Transformer
+    def initialize (raw_text)
+      @raw_text = raw_text
+    end
+
+    # FIXME: Implement this method
+    def transform
+      @raw_text
+    end
   end
 end
