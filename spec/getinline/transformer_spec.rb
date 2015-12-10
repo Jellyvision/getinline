4cbb1b674dd10d3248135a0fab39ca757cce7f79
Jeremy Kahn <jeremy@jellyvision.com>
2015-12-10 17:40:24 -0600

    Move Transformer into its own file

---
+++ spec/getinline/transformer_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe Getinline do
+  context '#transform' do
+    context 'no erb tags' do
+      raw_basic = File.read('spec/fixtures/raw/basic.erb')
+      inlined_basic = File.read('spec/fixtures/inlined/basic.erb')
+
+      transformer = Getinline::Transformer.new(raw_basic)
+      transformed_text = transformer.transform
+
+      it 'renders inlined html' do
+        expect(transformed_text).to include(inlined_basic)
+      end
+    end
+
+    context 'with erb tags' do
+      raw_with_tags = File.read('spec/fixtures/raw/with-tags.erb')
+      inlined_with_tags = File.read('spec/fixtures/inlined/with-tags.erb')
+
+      transformer = Getinline::Transformer.new(raw_with_tags)
+      transformed_text = transformer.transform
+
+      it 'renders inlined html' do
+        expect(transformed_text).to include(inlined_with_tags)
+      end
+    end
+  end
+end
--- spec/getinline_spec.rb
+++ spec/getinline_spec.rb
@@ -4,34 +4,4 @@ describe Getinline do
   it 'has a version number' do
     expect(Getinline::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(Getinline::Transformer).to be_a(Class)
-  end
-
-  context '#transform' do
-    context 'no erb tags' do
-      raw_basic = File.read('spec/fixtures/raw/basic.erb')
-      inlined_basic = File.read('spec/fixtures/inlined/basic.erb')
-
-      transformer = Getinline::Transformer.new(raw_basic)
-      transformed_text = transformer.transform
-
-      it 'renders inlined html' do
-        expect(transformed_text).to include(inlined_basic)
-      end
-    end
-
-    context 'with erb tags' do
-      raw_with_tags = File.read('spec/fixtures/raw/with-tags.erb')
-      inlined_with_tags = File.read('spec/fixtures/inlined/with-tags.erb')
-
-      transformer = Getinline::Transformer.new(raw_with_tags)
-      transformed_text = transformer.transform
-
-      it 'renders inlined html' do
-        expect(transformed_text).to include(inlined_with_tags)
-      end
-    end
-  end
 end
