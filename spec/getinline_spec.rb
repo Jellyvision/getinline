2ad62988f1ba39ccb25fdc5abcaf9da61a4e41e1
Jeremy Kahn <jeremy@jellyvision.com>
2015-12-10 10:40:39 -0600

    Add #transform spec for ERB tag support

--- spec/getinline_spec.rb
+++ spec/getinline_spec.rb
@@ -10,14 +10,28 @@ describe Getinline do
   end
 
   context '#transform' do
-    raw_basic = File.read('spec/fixtures/raw/basic.erb')
-    inlined_basic = File.read('spec/fixtures/inlined/basic.erb')
+    context 'no erb tags' do
+      raw_basic = File.read('spec/fixtures/raw/basic.erb')
+      inlined_basic = File.read('spec/fixtures/inlined/basic.erb')
 
-    transformer = Getinline::Transformer.new(raw_basic)
-    transformed_text = transformer.transform
+      transformer = Getinline::Transformer.new(raw_basic)
+      transformed_text = transformer.transform
 
-    it 'renders inlined html' do
-      expect(transformed_text).to include(inlined_basic)
+      it 'renders inlined html' do
+        expect(transformed_text).to include(inlined_basic)
+      end
+    end
+
+    context 'with erb tags' do
+      raw_with_tags = File.read('spec/fixtures/raw/with-tags.erb')
+      inlined_with_tags = File.read('spec/fixtures/inlined/with-tags.erb')
+
+      transformer = Getinline::Transformer.new(raw_with_tags)
+      transformed_text = transformer.transform
+
+      it 'renders inlined html' do
+        expect(transformed_text).to include(inlined_with_tags)
+      end
     end
   end
 end
