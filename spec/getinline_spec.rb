279fb3dd7f4851b420087542dc481055929579fb
Jeremy Kahn <jeremy@jellyvision.com>
2015-12-10 10:29:23 -0600

    Add basic #transform spec

--- spec/getinline_spec.rb
+++ spec/getinline_spec.rb
@@ -8,4 +8,16 @@ describe Getinline do
   it 'does something useful' do
     expect(Getinline::Transformer).to be_a(Class)
   end
+
+  context '#transform' do
+    raw_basic = File.read('spec/fixtures/raw/basic.erb')
+    inlined_basic = File.read('spec/fixtures/inlined/basic.erb')
+
+    transformer = Getinline::Transformer.new(raw_basic)
+    transformed_text = transformer.transform
+
+    it 'renders inlined html' do
+      expect(transformed_text).to include(inlined_basic)
+    end
+  end
 end
