952ca8f49328bad916f087936bdda479096996ed
Travis Pennetti <tpennetti@jellyvision.com>
2015-12-14 16:59:00 -0600

    Added premailer options from premailer/executor as a temporarysolution for interfacing with premailer

--- lib/getinline/transformer.rb
+++ lib/getinline/transformer.rb
@@ -5,8 +5,10 @@ TOKENIZED_ERB_FILE_NAME = "/tmp/tokenized.erb"
 
 module Getinline
   class Transformer
-    def initialize (raw_text)
+    def initialize (raw_text, options = {}, premailer_options = {})
       @raw_text = raw_text
+      @options = options
+      @premailer_options = premailer_options
     end
 
     def transform
@@ -18,8 +20,9 @@ module Getinline
 
       File.write(TOKENIZED_ERB_FILE_NAME, tokenized_text)
 
-      @premailer = Premailer.new(TOKENIZED_ERB_FILE_NAME, :warn_level => Premailer::Warnings::SAFE)
-      premailed_tokenized_text = @premailer.to_inline_css
+      @premailer = Premailer.new(TOKENIZED_ERB_FILE_NAME, @premailer_options)
+      premailed_tokenized_text = @options[:mode] == :txt ?
+        @premailer.to_plain_text : @premailer.to_inline_css
       premailed_text = premailed_tokenized_text.dup
 
       matches.each do |match|
